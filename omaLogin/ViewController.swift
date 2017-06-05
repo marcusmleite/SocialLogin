89f92330c855f2f0895cda540beb6a4c9bfad931
Rodrigo Vianna <rodrigogvianna@outlook.com>
2017-06-05 10:58:27 -0300

    Initial Commit

---
+++ omaLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  omaLogin
+//
+//  Created by Rodrigo Vianna on 05/06/17.
+//  Copyright © 2017 Rodrigo Vianna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
